c37018a3faefdf205933e062a86cdea77e45a9a9
Belingheri <riccardo.beli99@gmail.com>
2023-05-20 13:36:36 +0200

    edit trigger function

--- modules/refmint.mjs
+++ modules/refmint.mjs
@@ -1,30 +1,43 @@
-// const refmintSDK = require("refmint-sdk")
+import Refmint, { BaseURLOptions } from "refmint-sdk";
 
-// Trigger Refmint Event
-export async function triggerEvent(wallet_address) {
-  // // Set RefMint SDK
-  // const refmintClient = new refmintSDK.default.Game({
-  // 	apiKey: process.env.DEVELOPER_API_KEY,
-  // 	baseUrl: refmintSDK.BaseURLOptions.TESTNET
-  // });
+/**
+ * Trigger Refmint Event
+ * @param {string} player player joined address
+ * @param {BigInt} betSize amout of the bet
+ */
+export async function triggerEvent(player, betSize) {
+  return new Promise((res, rej) => {
+    // // Set RefMint SDK
+    const refmintClient = new Refmint.default.Game({
+      apiKey: process.env.DEVELOPER_API_KEY,
+      baseUrl: BaseURLOptions.TESTNET,
+    });
 
-  // // You can use the RefMint SDK, or call the RefMint Rest API
-  // // RefMint SDK (https://docs.refmint.xyz/refmint-sdk/sdk) 
-  // // github: https://github.com/ReferralOS/refmint-sdk
-  // // REST API endpoints: https://docs.refmint.xyz/refmint-sdk/sdk/endpoints
-  // //    - For REST API, x-api-key header required and Developer API Key
-  // refmintClient.triggerEvent(
-  //   process.env.PROJECT_ID,
-  //   process.env.CAMPAIGN_ID,
-  //   "Transfer", // Custom Event Name
-  //   "TRANSFER", // Custom Event Type
-  //   "ERC20",    // Custom Event Group
-  //   wallet_address
-  // ).then((resp) => {
-  //   //do something...
-  // }).catch(e => {
-  //   console.log(e);
-  // });
+    console.log(
+      `Start processing Transaction From: ${player} amout ${betSize}`
+    );
 
-  console.log(`Processing Transaction From: ${wallet_address}`);
+    // // You can use the RefMint SDK, or call the RefMint Rest API
+    // // RefMint SDK (https://docs.refmint.xyz/refmint-sdk/sdk)
+    // // github: https://github.com/ReferralOS/refmint-sdk
+    // // REST API endpoints: https://docs.refmint.xyz/refmint-sdk/sdk/endpoints
+    // //    - For REST API, x-api-key header required and Developer API Key
+    refmintClient
+      .triggerEvent(
+        process.env.PROJECT_ID,
+        process.env.CAMPAIGN_ID,
+        player,
+        "0xc3703065ac2dfaa94373834e8d6914faf67e969e650f780b8f282e014447c06e",
+        betSize.toString()
+      )
+      .then((resp) => {
+        console.log("resonse by refserver");
+        console.log(resp);
+        res();
+      })
+      .catch((e) => {
+        console.log(e);
+        rej(e);
+      });
+  });
 }
